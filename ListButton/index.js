c2079b52fd55a18d39daccbe950f06f12e09f16c
Andrew Orcilla <orcillaa@bellevuecollege.edu>
2018-10-10 11:29:35 -0700

    Upadated README file and ListButton Dir

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,8 +1,20 @@
 window.onload = () => {
+// Part I
     const userInput = document.getElementById('list-data');    
     const setTextButton = document.getElementById('set-text');
-
+    
     setTextButton.onclick = () => {
         userInput.value = 'The first item for my list';
     }
+//Part II
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
+    const listButton = document.getElementById('add-to-list');
+    
+    const setTextButton = document.getElementById('set-text');
+const userInput = document.getElementById('list-data');
+const listButton = document.getElementById('add-to-list');
+
 }
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ GitHub Pages Website For Prog109
 
 Test for Prog109
 
-I wrote this on Cloud9
+I wrote this on Cloud9.
+
+I wrote this in Visual studio in class.
