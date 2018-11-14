2f1f3a8c5bd1cf48514dfecae788bf642e04f596
Andrew Orcilla <orcillaa@bellevuecollege.edu>
2018-11-14 12:13:26 -0800

    added object-choices. html and js

---
+++ javascript-tests/object-choices.js
@@ -0,0 +1,58 @@
+function loader() {
+    const getNamesButton = document.getElementById('getNames');
+    const functionButton = document.getElementById('getFunctionObject');
+    getNamesButton.onclick = function() {
+        const name = myObject.getName();
+        console.log(name);
+        const getNameDisplay = document.getElementById('getNameDisplay');
+        getNameDisplay.textContent = name;
+
+        const fullName = myObject.getFullName();
+        console.log(fullName)
+        const getFullNameDisplay = document.getElementById('getFullNameDisplay');
+        getFullNameDisplay.textContent = fullName;
+
+    }
+
+    const functionObject = new FunctionObject();
+    console.log(functionObject.getFullName());
+    //console.log(functionObject.getName());
+    //functionButton.onclick = functionObject;
+        
+}
+
+window.onload = loader;
+
+// Key value pairs.
+// comma seperated list of key value pairs.
+// key building blocks for objects(two types of objects)
+const myObject = {
+    firstName: 'fred',
+    lastName: 'smith',
+    getName: function() {
+        return this.firstName + ' ' + this.lastName;
+    }
+};
+// capital letter you must call it with 'new' since you are using a prototype
+function FunctionObject() {
+    const firstName = 'sue';
+    const lastName = 'jones';
+
+    FunctionObject.prototype.getFullName = () => {
+        return firstName + ' ' + lastName;
+    }
+};
+
+myObject.middleName = 'barfoo';
+
+myObject.getFullName = function() {
+    return this.firstName + 
+        ' ' + this.middleName +
+        ' ' + this.lastName
+}
+
+console.log(myObject.firstName);
+console.log(myObject['firstName']);
+console.log(myObject.getName());
+console.log(myObject.middleName);
+console.log(myObject.getFullName());
