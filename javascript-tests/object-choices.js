32353380a3245776489b8e110342faa6579957c7
Andrew Orcilla <orcillaa@bellevuecollege.edu>
2018-11-19 12:16:40 -0800

    week 10 in class work

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,63 +1,63 @@
-/*function loader() {
-    const getNamesButton = document.getElementById('getNames');
-    const functionButton = document.getElementById('getFunctionObject');
-    getNamesButton.onclick = function() {
-        const name = myObject.getName();
-        console.log(name);
-        const getNameDisplay = document.getElementById('getNameDisplay');
-        getNameDisplay.textContent = name;
+// function loader() {
+//     const getNamesButton = document.getElementById('getNames');
+//     const functionButton = document.getElementById('getFunctionObject');
+//     getNamesButton.onclick = function() {
+//         const name = myObject.getName();
+//         console.log(name);
+//         const getNameDisplay = document.getElementById('getNameDisplay');
+//         getNameDisplay.textContent = name;
 
-        const fullName = myObject.getFullName();
-        console.log(fullName)
-        const getFullNameDisplay = document.getElementById('getFullNameDisplay');
-        getFullNameDisplay.textContent = fullName;
+//         const fullName = myObject.getFullName();
+//         console.log(fullName)
+//         const getFullNameDisplay = document.getElementById('getFullNameDisplay');
+//         getFullNameDisplay.textContent = fullName;
 
-    }
+//     }
 
-    const functionObject = new FunctionObject();
-    console.log(functionObject.getFullName());
-    //console.log(functionObject.getName());
-    //functionButton.onclick = functionObject;
+//     const functionObject = new FunctionObject();
+//     console.log(functionObject.getFullName());
+//     //console.log(functionObject.getName());
+//     //functionButton.onclick = functionObject;
         
-}
+// }
 
-window.onload = loader;
+// window.onload = loader;
 
-// Key value pairs.
-// comma seperated list of key value pairs.
-// key building blocks for objects(two types of objects)
-const myObject = {
-    firstName: 'fred',
-    lastName: 'smith',
-    getName: function() {
-        return this.firstName + ' ' + this.lastName;
-    }
-};
-// capital letter you must call it with 'new' since you are using a prototype
-function FunctionObject() {
-    const firstName = 'sue';
-    const lastName = 'jones';
+// // Key value pairs.
+// // comma seperated list of key value pairs.
+// // key building blocks for objects(two types of objects)
+// const myObject = {
+//     firstName: 'fred',
+//     lastName: 'smith',
+//     getName: function() {
+//         return this.firstName + ' ' + this.lastName;
+//     }
+// };
+// // capital letter you must call it with 'new' since you are using a prototype
+// function FunctionObject() {
+//     const firstName = 'sue';
+//     const lastName = 'jones';
 
-    FunctionObject.prototype.getFullName = () => {
-        return firstName + ' ' + lastName;
-    }
-};
+//     FunctionObject.prototype.getFullName = () => {
+//         return firstName + ' ' + lastName;
+//     }
+// };
 
-myObject.middleName = 'barfoo';
+// myObject.middleName = 'barfoo';
 
-myObject.getFullName = function() {
-    return this.firstName + 
-        ' ' + this.middleName +
-        ' ' + this.lastName
-}
+// myObject.getFullName = function() {
+//     return this.firstName + 
+//         ' ' + this.middleName +
+//         ' ' + this.lastName
+// }
 
-console.log(myObject.firstName);
-console.log(myObject['firstName']);
-console.log(myObject.getName());
-console.log(myObject.middleName);
-console.log(myObject.getFullName());*/
+// console.log(myObject.firstName);
+// console.log(myObject['firstName']);
+// console.log(myObject.getName());
+// console.log(myObject.middleName);
+// console.log(myObject.getFullName());
 
-// Object Choices Assignment
+// Object Choices Assignment   
 window.onload = () => {
     const simpleObjectAction = document.getElementById('simple-object-action');
     const functionObjectAction = document.getElementById('function-object-action');
@@ -68,58 +68,49 @@ window.onload = () => {
         console.log(simpleDisplay);
         const simpleObjectDisplay = document.getElementById('simple-object-display');
         simpleObjectDisplay.textContent = simpleDisplay;
-    }
+    };
 
-    //i dont understand how to make this work
     functionObjectAction.onclick = () => {
         const name = new FunctionObject();
         name.sayName();
         const functionObjectDisplay = document.getElementById('function-object-display');
         functionObjectDisplay.textContent = name;
-    }
+    };
 
     customClassAction.onclick = () => {
         const name = myClass.sayName();
         console.log(name);
         const customClassDisplay = document.getElementById('custom-class-display');
         customClassDisplay.textContent = name;
-    }
-}
-let simpleObject = {
-    sayName: 'Simple Object'
+    };
 };
-
+//Simple Object
+let simpleObject = {
+    sayName: 'Simple Object',
+};
 simpleObject.dynamicMethod = () => {
     console.log('Dynamic Method');
 };
 
-//Im having trouble for what you are asking us to do for this part
+//Function Object
 function FunctionObject() {
-
 
     function privateFunction() {
         console.log('Private Function');
     };
 
-
     FunctionObject.prototype.sayName = () => {
         'use strict'
         privateFunction();
     };
-
 };
 
-
-
+//Custom Class
 class CustomClass {
     sayName() {
         return 'Custom class'
     }
 };
-
-
-
-
 console.log(simpleObject.sayName);
 simpleObject.dynamicMethod();
 let myObject = new FunctionObject();
---
+++ javascript-tests/week10.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    const objectLiteralAction = document.getElementById('object-literal-action');
+    const numbersAction = document.getElementById('numbers-action');
+    
+    numbersAction.onclick = () => {
+        const numbersDisplay = document.getElementById('numbers-display');
+        numbersDisplay.textContent = numbers;
+        for (let number of numbers) {
+            const li = document.createElement("li"); //creates list item 
+            li.appendChild(document.createTextNode(number)); //set text for the list item
+            numbersDisplay.appendChild(li); //append it to list (insert it in the array)
+        }
+    }
+    objectLiteralAction.onclick = () => {
+        console.log(objectLiteral.getName());
+        const objectLiteralDisplay = document.getElementById('object-literal-display');
+        objectLiteralDisplay.textContent = objectLiteral.getName();
+    }
+}
+
+const numbers = [2, 1, 3];
+
+const objectLiteral = {
+    objectName: 'object literal is my name',
+    //below, we are declaring a method and the method "returns" the objectName string.
+    getName: function() {
+        return this.objectName
+    }
+}
